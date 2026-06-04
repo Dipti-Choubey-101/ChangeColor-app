63835b3584fc8a241aa02a98e80f0d85c868d11e
Dipti-Choubey-101 <diptichoubey101@gmail.com>
2026-06-04 14:27:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/changecolor/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.changecolor
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var view: View
+    private lateinit var button: FloatingActionButton
+    private val colors=arrayOf(Color.RED,Color.WHITE,Color.GRAY,Color.GREEN,Color.MAGENTA,Color.YELLOW)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        view=findViewById(R.id.view)
+        button=findViewById(R.id.button_change)
+
+        button.setOnClickListener {
+            view.setBackgroundColor(colors[Random.nextInt(colors.size)])
+        }
+    }
+}
